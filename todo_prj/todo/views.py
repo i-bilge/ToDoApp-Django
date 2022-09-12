7445742ca8708d3d53c2098ad464538bb730f885
i-bilge <iismailbilge@gmail.com>
2022-09-12 16:36:02 +0200

    form template for add page is created

--- todo_prj/todo/views.py
+++ todo_prj/todo/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
+from django.urls import reverse_lazy
 
 # # functionbased
 # def home(request):
@@ -15,3 +17,7 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task')
