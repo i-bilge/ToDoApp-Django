3f2b19d10c39cea083c572e8c9af604893178b9d
i-bilge <iismailbilge@gmail.com>
2022-09-13 13:25:53 +0200

    login page is added.

--- todo_prj/todo/views.py
+++ todo_prj/todo/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+
 from .models import Task
 from django.urls import reverse_lazy
 
@@ -29,3 +31,10 @@ class TaskDelete(DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy("task")
+class CustomLoginView(LoginView):
+    template_name = 'todo/login.html'
+    fields = "__all__"
+    redirect_authenticated_user: False
+
+    def get_success_url(self):
+        return reverse_lazy('task')
